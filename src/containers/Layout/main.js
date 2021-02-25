7678b9e6e9823412958fd0d55ae03fc9a0f9b9e7
Mathew Pauly <mathewp.info@gmail.com>
2021-02-25 22:18:14 +0530

    reverted layout

---
+++ src/containers/Layout/main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useIntl } from "react-intl";
+import { Route, Switch, Redirect } from "react-router-dom";
+import FaIcons from "../../components/fa-icons";
+
+import Configuration from "../Configuration";
+import Projects from "../Projects";
+import NotFound from "../NotFound";
+
+const Main = ({
+  collapsed,
+  rtl,
+  image,
+  handleToggleSidebar,
+  handleCollapsedChange,
+  handleRtlChange,
+  handleImageChange,
+}) => {
+  const intl = useIntl();
+  return (
+    <main>
+      <div className="btn-toggle" onClick={() => handleToggleSidebar(true)}>
+        <FaIcons icon="user" />
+      </div>
+
+      <div className="main-container">
+        <Switch>
+          <Route exact path="/App/Configuration" component={Configuration} />
+          <Route exact path="/App/Projects" component={Projects} />
+          
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+
+      <footer>
+        <small>© 2021 SES Master Tool</small>
+        <br />
+      </footer>
+    </main>
+  );
+};
+
+export default Main;
