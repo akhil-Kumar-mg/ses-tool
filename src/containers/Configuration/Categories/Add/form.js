6917cd19a952b70185aa42c296b483326d6e6d38
Mathew Pauly <mathewp.info@gmail.com>
2021-02-26 19:32:52 +0530

    category add more items

--- src/containers/Configuration/Categories/Add/form.js
+++ src/containers/Configuration/Categories/Add/form.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 
 function Form() {
+
+  const [items, setItems] = useState([]);
+  const onAdd = () =>{
+    const _items = [...items];
+    _items.push(items.length + 1);
+    setItems(_items);
+  }
+  const onDelete = () =>{
+    const _items = [...items];
+    _items.pop();
+    setItems(_items);
+  }
   return (
+
     <>
       <form>
         <div className="form-group">
@@ -11,6 +24,38 @@ function Form() {
             placeholder="Category name"
           />
         </div>
+
+        {items.map((item) => {
+          return (
+            <>
+              <div className="form-group">
+                <label>Sub-category name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Placeholder text"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Applicable commercial units</label>
+                <select className="form-control">
+                  <option>Placeholder text</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+              <div className="center">
+                <a href="javascript:void(0)" className="delete" onClick={onDelete}>Delete</a>
+              </div>
+              <div className="hr-dashed"></div>
+            </>
+          );
+        })}
+
         <div className="form-group">
           <label>Sub-category name</label>
           <input
@@ -21,9 +66,7 @@ function Form() {
         </div>
 
         <div className="form-group">
-          <label>
-            Applicable commercial units
-          </label>
+          <label>Applicable commercial units</label>
           <select className="form-control">
             <option>Placeholder text</option>
             <option>1</option>
@@ -34,7 +77,7 @@ function Form() {
           </select>
         </div>
         <div className="center">
-          <a href="javascript:void(0)">Add another</a>
+          <a href="javascript:void(0)" onClick={onAdd}>Add another</a>
         </div>
       </form>
     </>
