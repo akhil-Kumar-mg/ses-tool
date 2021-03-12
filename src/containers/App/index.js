571096b4bf997a9a6ee39bac8b880d3b8ba311df
Mathew Pauly <mathewp.info@gmail.com>
2021-03-12 23:48:14 +0530

    reduced screen resolution

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.scss';
 import { IntlProvider } from 'react-intl';
 import messages from '../../constants/messages';
@@ -7,7 +7,6 @@ import Layout from '../Layout';
 
 function App() {
   const [locale, setLocale] = useState('en');
-
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
       <Layout setLocale={setLocale} />
