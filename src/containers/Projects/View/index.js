8d5647715315e5f7090f342bdc73c32e11a56773
akhil-kumar-mg <connectakhil.kumar@gmail.com>
2021-05-03 10:05:16 +0530

    Added monthly data to p/l

--- src/containers/Projects/View/index.js
+++ src/containers/Projects/View/index.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import "./style.scss";
+import useNotify from "../../../actions/Toast";
 import FaIcons from "../../../components/fa-icons";
 import Grid from "../../../components/Grid";
-import schema from "./metadata/schema.json";
-import data from "./metadata/data.json";
-import useNotify from "../../../actions/Toast";
 import { getForecasts } from "../service";
+import monthlySchema from "./metadata/schema-monthly-forecast.json";
+import schema from "./metadata/schema.json";
 import { getPL } from "./service";
+import "./style.scss";
 
 function ProjectsPL(props) {
   const history = useHistory();
   const { notify } = useNotify();
   const [forecast, setForecast] = useState([]);
+  const [currentForecast, setCurrentForecast] = useState([]);
   const [pl, setPL] = useState([]);
+  const [showMonthlyDetails, setShowMonthlyDetails] = useState(false);
+  const [monthlyData, setMonthlyData] = useState([]);
 
   useEffect(() => {
     onLoad();
@@ -33,10 +36,52 @@ function ProjectsPL(props) {
     }
   };
 
+  const handleSchema = () => {
+    monthlySchema.columns = [];
+    monthlySchema.columns.push({
+      name: "PARAMETERS",
+      field: "parameter",
+    });
+    for (let i = 1; i <= 12; i++) {
+      monthlySchema.columns.push({
+        name: "MONTH" + " " + i,
+        field: i,
+      });
+    }
+  };
+
+  const handleMonthlyData = () => {
+    let rows = [];
+    let parameters = [
+      "monthly_catchup_hours",
+      "monthly_channel_count",
+      "monthly_channel_hours",
+      "monthly_content_consumed_catchup_gb",
+      "monthly_content_consumed_gb",
+      "monthly_content_consumed_linear_gb",
+      "monthly_content_consumed_vod_gb",
+      "monthly_content_stored_origin_gb",
+      "monthly_impression",
+      "monthly_origin_hit_gb",
+      "monthly_subscriber_count",
+      "monthly_vod_hours",
+    ];
+    for (let j = 0; j < parameters.length; j++) {
+      let row = {};
+      row["parameter"] = parameters[j];
+      for (let i = 1; i <= currentForecast.number_of_months; i++) {
+        row[i] = currentForecast[parameters[j]][i];
+      }
+      rows.push(row);
+    }
+    setMonthlyData(rows);
+  };
+
   const onLoad = () => {
     getForecasts(props.match.params.projectId)
       .then((res) => {
         setForecast(res);
+        setCurrentForecast(res[0]);
         loadPL(props.match.params.projectId, res[0].id);
       })
       .catch((err) => notify("Oops! Failed to fetch forecasts.", "error"));
@@ -58,8 +103,15 @@ function ProjectsPL(props) {
       .catch((err) => notify("Oops! Failed to fetch P/L's.", "error"));
   };
 
-  const onScenarioChange = (forecastId) => {
-    loadPL(props.match.params.projectId, forecastId);
+  const onScenarioChange = (index) => {
+    setCurrentForecast(forecast[index]);
+    loadPL(props.match.params.projectId, forecast[index].id);
+  };
+
+  const handleMonthlyDetails = () => {
+    setShowMonthlyDetails(true);
+    handleSchema();
+    handleMonthlyData();
   };
 
   return (
@@ -78,25 +130,27 @@ function ProjectsPL(props) {
             P/L - {props.location.state.projectName}
           </h3>
           <div>
-            {/* <button
+            <button
               type="button"
               className="btn btn-primary"
-              // onClick={handleShow}
+              onClick={handleMonthlyDetails}
             >
               VIEW MONTHLY
-            </button> */}
+            </button>
             <div className="form-group">
               <label>P/L Scenarios</label>
               <select
                 className="form-control"
                 value={forecast.name}
-                onChange={(e) => onScenarioChange(e.target.value)}
+                onChange={(e) => {
+                  onScenarioChange(e.target.value);
+                }}
               >
                 {forecast &&
                   forecast.length &&
-                  forecast.map((item) => {
+                  forecast.map((item, index) => {
                     return (
-                      <option key={item} value={item.id}>
+                      <option key={item} value={index}>
                         {item.name}
                       </option>
                     );
@@ -106,7 +160,19 @@ function ProjectsPL(props) {
           </div>
         </div>
         <div className="sub-container">
-          <Grid data={pl} schema={schema} onChange={onGridChange} />
+          {showMonthlyDetails ? (
+            <>
+              <Grid
+                data={monthlyData}
+                schema={monthlySchema}
+                onChange={onGridChange}
+              />
+            </>
+          ) : (
+            <>
+              <Grid data={pl} schema={schema} onChange={onGridChange} />{" "}
+            </>
+          )}
         </div>
       </div>
     </>
