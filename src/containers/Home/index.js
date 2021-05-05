47bc8e4e1681a1c5457bf99a12d6a474ea9cb0fc
Mathew Pauly <mathewp.info@gmail.com>
2021-05-05 13:40:36 +0530

    added welcome screen + projects card alignment

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory, withRouter } from "react-router-dom";
 import "./style.scss";
 
 function Home() {
+  const history = useHistory();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      document.getElementById("splash-overlay").style.display ="none";
+    }, 3000)
+  },[])
   return (
     <>
-        <div className="center-logo">
-          <h1>Welcome to Ses Master tool </h1>
+      <div className="welcome">
+        <span id="splash-overlay" className="splash"></span>
+        <span id="welcome" className="z-depth-4"></span>
+
+        <div className="valign-wrapper">
+          <span className="container grey-text text-lighten-1 ">
+            <p className="flow-text">Welcome to</p>
+            <h1 className="title grey-text text-lighten-3">
+              ses tool
+            </h1>
+
+            <blockquote className="flow-text">
+              A project management portal
+            </blockquote>
+
+            <div className="center-align">
+              <button type="button" className="btn btn-danger" onClick={()=>{history.push(`/App/Configuration`);}}> Configuration</button>
+              <button type="button" className="btn btn-success" onClick={()=>{history.push(`/App/Projects`);}}> Projects</button>
+            </div>
+          </span>
         </div>
+
+       
+
+        
+      </div>
     </>
   );
 }
