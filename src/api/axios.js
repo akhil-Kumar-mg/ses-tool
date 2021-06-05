1407367c76f10fc4db5244cbd43dc18a68d6786f
Akhil Kumar <31155395+akhil-Kumar-mg@users.noreply.github.com>
2021-06-05 12:16:00 +0530

    Adding login api

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const baseURL = process.env.REACT_APP_API_URL;
 const baseHeaders = {
   Accept: "application/json",
   "Content-Type": "application/json",
+  Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImVtYWlsIjoiYWRtaW5Ac2VzLmNvbSIsImV4cCI6MTYyMjQ1MjY2NSwianRpIjoiZjgxMWRhOGUtZDBmOC00YjQ3LTk5MWItNWU5OTJmMTUzOThjIn0.vDj2BMVEg8b4-h_8-UUHJg_VzBcVdUQLWsbn1EfEB10"
 };
 
 const instance = axios.create({
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,63 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useHistory, withRouter } from "react-router-dom";
-import './style.scss';
+import "./style.scss";
 import useNotify from "../../actions/Toast";
-import logo from '../../assets/img/ses-logo.png';
+import logo from "../../assets/img/ses-logo.png";
 
 function Login() {
-    const history = useHistory();
-    const { notify } = useNotify();
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+  const history = useHistory();
+  const { notify } = useNotify();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const onLogin = () => {
+    if (username === "admin" && password === "password") {
+      history.push(`/App`);
+    } else {
+      notify("Incorrect username/password", "error");
+    }
+  };
 
-const onLogin = () =>{
+  return (
+    <div className="login-container container">
+      <div className="row">
+        <div className="col-lg-3 col-md-2"></div>
+        <div className="col-lg-6 col-md-8 login-box">
+          <div className="col-lg-12 login-key">
+            <img src={logo} alt="ses ovp master tool" />
+          </div>
 
-if(username === "admin" && password==="password"){
-
-    history.push(`/App`);
-}else{
-    notify("Incorrect username/password", "error");
-}
-
-}
-
-
-  return <div className="login-container container">
-        <div className="row">
-            <div className="col-lg-3 col-md-2"></div>
-            <div className="col-lg-6 col-md-8 login-box">
-                <div className="col-lg-12 login-key">
-                <img src={logo} alt="ses ovp master tool" />
+          <div className="col-lg-12 login-form">
+            <div className="col-lg-12 login-form">
+              <form>
+                <div className="form-group">
+                  <label className="form-control-label">USERNAME</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="form-group">
+                  <label className="form-control-label">PASSWORD</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
                 </div>
 
-                <div className="col-lg-12 login-form">
-                    <div className="col-lg-12 login-form">
-                        <form>
-                            <div className="form-group">
-                                <label className="form-control-label">USERNAME</label>
-                                <input type="text" className="form-control" value={username} onChange={(e)=>{setUsername(e.target.value)}} />
-                            </div>
-                            <div className="form-group">
-                                <label className="form-control-label">PASSWORD</label>
-                                <input type="password" className="form-control"  value={password} onChange={(e)=>{setPassword(e.target.value)}} />
-                            </div>
-
-                            <div className="col-lg-12 loginbttm">
-                                <div className="col-lg-6 login-btm login-text">
-
-                                </div>
-                                <div className="col-lg-6 login-btm login-button">
-                                    <button type="button" className="btn btn-secondary" onClick={onLogin}>LOGIN</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                <div className="col-lg-12 loginbttm">
+                  <div className="col-lg-6 login-btm login-text"></div>
+                  <div className="col-lg-6 login-btm login-button">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={onLogin}
+                    >
+                      LOGIN
+                    </button>
+                  </div>
                 </div>
-                <div className="col-lg-3 col-md-2"></div>
+              </form>
             </div>
-        </div></div>;
-  
+          </div>
+          <div className="col-lg-3 col-md-2"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
